69a8cfa47c8d241dc058629889c69c50f9188463
Sona Setiana <sonasetiana13@gmail.com>
2022-10-19 16:46:18 +0700

    add end implement sqlcipher

--- core/src/main/java/com/sonasetiana/core/data/di/LocalModule.kt
+++ core/src/main/java/com/sonasetiana/core/data/di/LocalModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,9 +19,12 @@ class LocalModule {
     @Singleton
     @Provides
     fun provideDataBase(@ApplicationContext context: Context) : FavoriteDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("nextgames".toCharArray())
+        val factory = SupportFactory(passphrase)
         return Room
             .databaseBuilder(context, FavoriteDatabase::class.java, "Favorite.db")
             .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 
